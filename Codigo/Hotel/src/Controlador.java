b31134379f9c4078649085af5e68e1c445f86de8
Jaime <jvelrui193@g.educaand.es>
2023-12-09 19:16:46 +0100

    Controlador Class para v0.2

--- Codigo/Hotel/src/Controlador.java
+++ Codigo/Hotel/src/Controlador.java
@@ -1,3 +1,123 @@
+import java.util.Scanner;
+
 public class Controlador {
+
+    private String sNombre;
+
+    Hotel miHotelControlador = new Hotel();
+
+    public Controlador(){
+        this.sNombre = "Controlador";
+    }
+
+    public boolean compCliente(){
+        for (int i = 0; i < miHotelControlador.aClientes.length; i++){
+            if ( miHotelControlador.aClientes[i] == null){
+                String dni, nombre, apellidos, direccion, email, telefono, fechaNac;
+                int edad;
+
+                Scanner sc = new Scanner(System.in);
+                System.out.println("DNI sin guion");
+                dni = sc.nextLine();
+                System.out.println("Nombre");
+                nombre = sc.nextLine();
+                System.out.println("Apellidos");
+                apellidos = sc.nextLine();
+                System.out.println("Direccion");
+                direccion = sc.nextLine();
+                System.out.println("Email");
+                email = sc.nextLine();
+                System.out.println("Telefono");
+                telefono = sc.nextLine();
+                System.out.println("Edad");
+                edad = Integer.valueOf(sc.nextLine());
+                System.out.println("Fecha Nac XX/XX/XXXX");
+                fechaNac= sc.nextLine();
+
+                miHotelControlador.addCliente(nombre, dni, apellidos, direccion, email, telefono, edad, fechaNac, i);
+                return true;
+                
+            }
+        }
+        return false;
+    }
     
+    public String compRemoveCliente(){
+        if (miHotelControlador.aClientes[0] == null){
+            return "No hay ningun cliente dado de alta todavía";
+            
+        } else {
+            String dni;
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Introduzca el DNI del cliente que desea eliminar");
+            dni = sc.nextLine();
+
+            if (miHotelControlador.removeCliente(dni)){
+                return "Cliente borrado";
+                
+            } else {
+                return "El DNI introducido no coincide con ninguno de los clientes";
+
+            }
+        }
+    }
+
+    public String showCliente(){
+        if (miHotelControlador.aClientes[0] == null){
+            return "No hay clientes en el hotel";
+            
+        } else{
+            Scanner sc = new Scanner(System.in);
+            System.out.println("DNI del cliente del que quiera hacer la consulta");
+            String dni = sc.nextLine();
+
+            return miHotelControlador.showCliente(dni);
+        }
+    }
+
+    
+    public boolean compPromocion(){
+        for (int i = 0; i < miHotelControlador.aPromociones.length; i++){
+            if ( miHotelControlador.aPromociones[i] == null){
+                int edadMax, edadMin, descuento, duracion;
+
+                Scanner sc = new Scanner(System.in);
+                System.out.println("DNI sin guion");
+                edadMax = Integer.valueOf(sc.nextLine());
+                System.out.println("Nombre");
+                edadMin = Integer.valueOf(sc.nextLine());
+                System.out.println("Apellidos");
+                descuento = Integer.valueOf(sc.nextLine());
+                System.out.println("Direccion");
+                duracion = Integer.valueOf(sc.nextLine());
+                
+                miHotelControlador.addPromo(i + 1, edadMax, edadMin, descuento, duracion, i);
+                sc.close();
+
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public String compRemovePromo(){
+        if (miHotelControlador.aClientes[0] == null){
+            return "No hay ninguna Promocion actualmente";
+            
+        } else {
+            int codPromocion;
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Introduzca el Codigo de la Promocion que desea eliminar");
+            codPromocion = Integer.valueOf(sc.nextLine());
+
+            if (miHotelControlador.removePromo(codPromocion)){
+                return "Promocion borrada";
+                
+            } else {
+                return "El Codigo introducido no coincide con ninguna Promocion";
+
+            }
+        }
+    }
+
 }
